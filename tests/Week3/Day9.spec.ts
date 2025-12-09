516feaf27d54d49895964d89f7e4a7dabbefa6a7
Kumar PC <kumarmunusamy0506@gmail.com>
2025-12-09 07:12:35 +0530

    Add Playwright tests for handling multiple dropdowns and creating a lead in TestLeaf CRM

--- tests/Week3/Day9.spec.ts
+++ tests/Week3/Day9.spec.ts
@@ -1,21 +1,33 @@
-import { test, expect } from '@playwright/test';
+import {test} from '@playwright/test';
 
-test(`Create a Lead in TestLeaf CRM`, async ({page}) =>{
-    page.goto('http://leaftaps.com/opentaps/control/main')
-    await page.locator(`//p[@class="top"]//input`).fill(`Demosalesmanager`)
-    await page.locator(`//input[@id="password"]`).fill(`crmsfa`)
-    await page.locator(`//p/input[@class="decorativeSubmit"]`).click()
-    await page.locator(`//div[@id='button']/a`).click()
-    await page.locator(`//a[text()="Leads"]`).click()
-    await page.locator(`//a[text()="Create Lead"]`).click()
-    await page.locator(`//input[@id='createLeadForm_companyName']`).fill(`TestLeaf`)
-    await page.locator(`//input[@id='createLeadForm_firstName']`).fill(`Kumar`)
-    await page.locator(`//input[@id='createLeadForm_lastName']`).fill(`M`)
-    const dropdown = page.locator(`#createLeadForm_marketingCampaignId>option`)
-    const countdropdown = await dropdown.count() 
-    console.log("The total number of dropdowns are: "+countdropdown);
-    for(let i=0;i<countdropdown;i++){
-        const dropdowntext = await dropdown.nth(i).textContent()
-        console.log("The dropdown values are: "+dropdowntext);
+test('Handle Multiple types of Dropdowns', async ({page}) => {
+    await page.goto('https://leafground.com/select.xhtml')  
+    await page.selectOption(`select[class="ui-selectonemenu"]`,{label:`Playwright`})  
+    const favoriteAutomationTool = page.locator(`select[class="ui-selectonemenu"]>option`)
+    const count = await favoriteAutomationTool.count()
+    console.log("The total number of favoriteAutomationTool dropdown options are: "+count);
+    for(let i=0;i<count;i++){
+      const favoriteAutomationToolDropdowntext =  await favoriteAutomationTool.nth(i).innerText()
+      console.log("The favoriteAutomationTool dropdown options are: "+favoriteAutomationToolDropdowntext);
     }
+    await page.locator(`//label[contains(text(),"Select Country")]`).click()
+    await page.locator(`//li[contains(text(),"India")]`).click()
+    await page.locator(`//button[contains(@class,"ui-autocomplete-dropdown")]`).click()
+    await page.locator(`//li[contains(text(),'Playwright')]`).click()
+    await page.locator(`//button[contains(@class,"ui-autocomplete-dropdown")]`).click()
+    await page.locator(`//li[contains(text(),'Selenium')]`).click()
+    await page.locator(`//button[contains(@class,"ui-autocomplete-dropdown")]`).click()
+    await page.locator(`//li[contains(text(),'PostMan')]`).click()
+    await page.locator(`//label[contains(text(),'Select Language')]`).click()
+    await page.locator(`//li[contains(text(),'Tamil')]`).click()
+    const languages = page.locator(`select[id="j_idt87:lang_input"]>option`)
+    const countlanguages = await languages.count()
+    console.log("The total number of dropdowns languages are: "+countlanguages);
+    for(let i=0;i<countlanguages;i++){
+    const languageText = await languages.nth(i).innerText()
+    console.log("Language dropdown option are: "+languageText);
+    }
+    await page.locator(`//label[contains(text(),'Select Values')]`).click()
+    await page.locator(`//li[contains(text(),'ஒன்று')]`).click()
+    await page.waitForTimeout(5000);
 });
