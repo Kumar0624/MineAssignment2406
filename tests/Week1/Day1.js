b9e61974f823fd9adb3676a674378da7b0db636a
Kumar PC <kumarmunusamy0506@gmail.com>
2026-01-15 18:37:15 +0530

    Remove client_id and client_secret from qa.env and comment them in Day1.js for security reasons

--- tests/Week1/Day1.js
+++ tests/Week1/Day1.js
@@ -19,5 +19,6 @@ console.log(hasPlaywright)
 console.log(typeof hasPlaywright)
 
 
-
+// client_id=3MVG9VMBZCsTL9hnYaVamF_yN9dU9vnyYTjdMcnDvjqPwwaBINmmrxc5nreM3diNAwBKSsbWXhbOweaL8rdNB
+// client_secret=045B87EED90898F6D00920E973955E048E340B15506294A39847C4C824C8E260
 
